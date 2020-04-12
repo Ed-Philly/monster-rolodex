10352fc3ab1597fabdfdc7333393640854c6ca7e
Egle kniukstaite <eglekniukstaite@egles-air.lan>
2020-04-12 03:37:09 +0100

    Updates

---
+++ precache-manifest.cdabff6f6f6a5187773d32ecd2bf40ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca864e3725d9650960bca0ce2fed0d4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "166995f37a7924c9021a",
+    "url": "/monster-rolodex/static/css/main.ab434582.chunk.css"
+  },
+  {
+    "revision": "12b321673ede22fdaecb",
+    "url": "/monster-rolodex/static/js/2.9f3535e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monster-rolodex/static/js/2.9f3535e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166995f37a7924c9021a",
+    "url": "/monster-rolodex/static/js/main.d740debe.chunk.js"
+  },
+  {
+    "revision": "e4eba3394aec38fdb39c",
+    "url": "/monster-rolodex/static/js/runtime-main.2047114a.js"
+  }
+]);
